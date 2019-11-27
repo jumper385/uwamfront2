2a7b6747eace6667ffc4a2dba09d4a0d9e1ee629
jumper385 <henrychen385@gmail.com>
2019-11-27 10:20:44 +0800

    added environment variables and boilerplate express

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const PORT = process.env.PORT || 8080
+
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req,res) => res.json({message:'hello world'}))
+
+app.listen(PORT)
